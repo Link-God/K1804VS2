89fcf5f28fc0aeb8ac6868bff4609c7983440e54
Link <panzar24@bk.ru>
2020-06-05 01:21:55 +0300

    Добавьте файлы проекта.

---
+++ K1804BC2.cpp
@@ -0,0 +1,682 @@
+// MT1804BC1.cpp: определяет экспортированные функции для приложения DLL.
+//
+
+#include "pch.h"
+#include "K1804BC2.h"
+#include "logger.h"
+
+uint8_t K1804BC2::genValue(IDSIMPIN** pins, size_t n, size_t offset)
+{
+	uint8_t res = 0;
+	for (size_t i = 0; i < n; ++i)
+		res |= isHigh(pins[offset + i]) << i;
+	return res;
+}
+
+
+// линковка выходов схемы в Proteus и в коде
+VOID K1804BC2::setup(IINSTANCE* instance, IDSIMCKT* dsimckt)
+{
+	_inst = instance;
+	_ckt = dsimckt;
+
+	char buffer[16];
+	for (size_t i = 0; i < REGISTER_SIZE; ++i)
+	{
+		sprintf_s(buffer, "A%d", i);
+		_pin_A[i] = _inst->getdsimpin(buffer, true);
+	}
+	for (size_t i = 0; i < REGISTER_SIZE; ++i)
+	{
+		sprintf_s(buffer, "B%d", i);
+		_pin_B[i] = _inst->getdsimpin(buffer, true);
+	}
+	for (size_t i = 0; i < REGISTER_SIZE; ++i)
+	{
+		sprintf_s(buffer, "DA%d", i);
+		_pin_DA[i] = _inst->getdsimpin(buffer, true);
+	}
+	for (size_t i = 0; i < REGISTER_SIZE; ++i)
+	{
+		sprintf_s(buffer, "DB%d", i);
+		_pin_DB[i] = _inst->getdsimpin(buffer, true);
+	}
+	for (size_t i = 0; i < PIN_I_SIZE; ++i)
+	{
+		sprintf_s(buffer, "I%d", i);
+		_pin_I[i] = _inst->getdsimpin(buffer, true);
+	}
+
+	_pin_PQ0 = _inst->getdsimpin(const_cast<CHAR*>("PQ0"), true);
+	_pin_PQ3 = _inst->getdsimpin(const_cast<CHAR*>("PQ3"), true);
+	_pin_PR0 = _inst->getdsimpin(const_cast<CHAR*>("PR0"), true);
+	_pin_PR3 = _inst->getdsimpin(const_cast<CHAR*>("PR3"), true);
+	_pin_T = _inst->getdsimpin(const_cast<CHAR*>("T"), true);
+	_pin_OEB = _inst->getdsimpin(const_cast<CHAR*>("OEB"), true);
+	_pin_EA = _inst->getdsimpin(const_cast<CHAR*>("EA"), true);
+	_pin_IEN = _inst->getdsimpin(const_cast<CHAR*>("IEN"), true);
+	_pin_C0 = _inst->getdsimpin(const_cast<CHAR*>("C0"), true);
+	_pin_OEY = _inst->getdsimpin(const_cast<CHAR*>("OEY"), true);
+	_pin_LSS = _inst->getdsimpin(const_cast<CHAR*>("LSS"), true);
+	_pin_W_MSS = _inst->getdsimpin(const_cast<CHAR*>("W/MSS"), true);
+	_pin_WE = _inst->getdsimpin(const_cast<CHAR*>("WE"), true);
+
+
+	for (size_t i = 0; i < REGISTER_SIZE; ++i)
+	{
+		sprintf_s(buffer, "Y%d", i);
+		_pin_Y[i] = _inst->getdsimpin(buffer, true);
+		//setState(0, _pin_Y[i], false);
+	}
+	// выход флагов
+	_pin_C4 = _inst->getdsimpin(const_cast<CHAR*>("C4"), true);
+	setState(0, _pin_C4, 0);
+	_pin_Z = _inst->getdsimpin(const_cast<CHAR*>("Z"), true);
+	setState(0, _pin_Z, 0);
+	_pin_F3 = _inst->getdsimpin(const_cast<CHAR*>("F3"), true);
+	setState(0, _pin_F3, 0);
+	_pin_P_OVR = _inst->getdsimpin(const_cast<CHAR*>("P/OVR"), true);
+	setState(0, _pin_P_OVR, 0);
+	_pin_G_N = _inst->getdsimpin(const_cast<CHAR*>("G/N"), true);
+	setState(0, _pin_G_N, 0);
+}
+
+// получение данных со всех входов
+K1804BC2::CommandFields* K1804BC2::getCommand()
+{
+	// TODO тож самое почти, но пины другие
+}
+
+void K1804BC2::__download__000(const CommandFields* cmd, Operands* ops, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__download__001(const CommandFields* cmd, Operands* ops, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__download__01X(const CommandFields* cmd, Operands* ops, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__download__100(const CommandFields* cmd, Operands* ops, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__download__101(const CommandFields* cmd, Operands* ops, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__download__11X(const CommandFields* cmd, Operands* ops, ILogger* log)
+{
+	// TODO
+}
+
+// Загрузка операндов
+K1804BC2::Operands* K1804BC2::getOperands(const CommandFields* cmd, ILogger* log)
+{
+	if (cmd == nullptr)
+	{
+		return nullptr;
+	}
+	Operands* ops = new Operands();
+	// TODO проверить так ли нужно вызывавать
+	switch (cmd->From)
+	{
+		// 000: R=POH(A), S=PQ
+	case 0:
+		__download__000(cmd, ops, log);
+		break;
+		// 001: R=POH(A), S=POH(B)
+	case 1:
+		__download__001(cmd, ops, log);
+		break;
+		// 010: R=0, S=PQ
+	case 2:
+		__download__01X(cmd, ops, log);
+		break;
+		// 011: R=0, S=POH(B)
+	case 3:
+		__download__01X(cmd, ops, log);
+		break;
+		// 100: R=0, S=POH(A)
+	case 4:
+		__download__100(cmd, ops, log);
+		break;
+		// 101: R=D, S=POH(A)
+	case 5:
+		__download__101(cmd, ops, log);
+		break;
+		// 110: R=D, S=PQ
+	case 6:
+		__download__11X(cmd, ops, log);
+		break;
+		// 111: R=D, S=0
+	case 7:
+		__download__11X(cmd, ops, log);
+		break;
+	}
+	return ops;
+}
+
+void K1804BC2::__alu__0000(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__alu__0001(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__alu__0010(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__alu__0011(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__alu__0100(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__alu__0101(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__alu__0110(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	//I0=0 -> error (reserved func)
+	// TODO
+}
+
+void K1804BC2::__alu__0111(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	//I0=0 -> error (reserved func)
+	// TODO
+}
+
+void K1804BC2::__alu__1000(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	//I0=0 -> error (reserved func)
+	// TODO
+}
+
+void K1804BC2::__alu__1001(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__alu__1010(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__alu__1011(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__alu__1100(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__alu__1101(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__alu__1110(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__alu__1111(bool c0, const Operands* ops, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+K1804BC2::ALUReasult* K1804BC2::ALU(bool c0, uint8_t code, const Operands* ops, ILogger* log)
+{
+	if (ops == nullptr)
+	{
+		return nullptr;
+	}
+	auto res = new ALUReasult();
+	// TODO : исключения в некторых случаях в зависимотси от I0
+	switch (code)
+	{
+		// I0 = 0 -> специальный функции
+		// I0 = 1 -> 1111
+	case 0:
+		__alu__0000(c0, ops, res, log);
+		break;
+		//S - R - 1 + C0
+	case 1:
+		__alu__0001(c0, ops, res, log);
+		break;
+		//R - S - 1 + C0
+	case 2:
+		__alu__0010(c0, ops, res, log);
+		break;
+		//R + S + C0
+	case 3:
+		__alu__0011(c0, ops, res, log);
+		break;
+		//S + C0
+	case 4:
+		__alu__0100(c0, ops, res, log);
+		break;
+		//~S + C0
+	case 5:
+		__alu__0101(c0, ops, res, log);
+		break;
+		//R + C0
+	case 6:
+		__alu__0110(c0, ops, res, log);
+		break;
+		//~R + C0
+	case 7:
+		__alu__0111(c0, ops, res, log);
+		break;
+		//0000
+	case 8:
+		__alu__1000(c0, ops, res, log);
+		break;
+		//~R & S
+	case 9:
+		__alu__1001(c0, ops, res, log);
+		break;
+		//~(R ^ S)
+	case 10:
+		__alu__1010(c0, ops, res, log);
+		break;
+		//R ^ S
+	case 11:
+		__alu__1011(c0, ops, res, log);
+		break;
+		//R & S
+	case 12:
+		__alu__1100(c0, ops, res, log);
+		break;
+		//~(R | S)
+	case 13:
+		__alu__1101(c0, ops, res, log);
+		break;
+		//~(R & S)
+	case 14:
+		__alu__1110(c0, ops, res, log);
+		break;
+		//R | S
+	case 15:
+		__alu__1111(c0, ops, res, log);
+		break;
+	}
+	return res;
+}
+
+
+void K1804BC2::__load__0000(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__0001(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__0010(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__0011(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__0100(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__0101(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+
+void K1804BC2::__load__0110(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__0111(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__1000(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__1001(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__1010(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__1011(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__1100(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__1101(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__1110(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__load__1111(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__special__0000(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__special__0010(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__special__0100(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__special__0101(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__special__0110(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__special__1000(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__special__1010(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__special__1100(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+void K1804BC2::__special__1110(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	// TODO
+}
+
+
+void K1804BC2::load(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	setState(_time, _pin_PR3, 0);
+	setState(_time, _pin_PR0, 0);
+	setState(_time, _pin_PQ3, 0);
+	setState(_time, _pin_PQ0, 0);
+	switch (cmd->To)
+	{
+		//arith F/2 -> Y 
+	case 0:
+		__load__0000(cmd, res, log);
+		break;
+		//log F/2 -> Y
+	case 1:
+		__load__0001(cmd, res, log);
+		break;
+		//arith F/2 -> Y  log Q/2 -> Q 
+	case 2:
+		__load__0010(cmd, res, log);
+		break;
+		//log F/2 -> Y  log Q/2 -> Q 
+	case 3:
+		__load__0011(cmd, res, log);
+		break;
+		// F -> Y Q-hold
+	case 4:
+		__load__0100(cmd, res, log);
+		break;
+		// F -> Y log Q/2 -> Q
+	case 5:
+		__load__0101(cmd, res, log);
+		break;
+		// F -> Y F -> Q (W = 1)
+	case 6:
+		__load__0110(cmd, res, log);
+		break;
+		// F -> Y F -> Q (W = 0)
+	case 7:
+		__load__0111(cmd, res, log);
+		break;
+		//arith 2F -> Y Q-hold
+	case 8:
+		__load__1000(cmd, res, log);
+		break;
+		// log 2F -> Y Q-hold
+	case 9:
+		__load__1001(cmd, res, log);
+		break;
+		// arith 2F -> Y  log 2Q -> Q
+	case 10:
+		__load__1010(cmd, res, log);
+		break;
+		// log 2F -> Y log 2Q -> Q
+	case 11:
+		__load__1011(cmd, res, log);
+		break;
+		// F -> Y log Q-hold
+	case 12:
+		__load__1100(cmd, res, log);
+		break;
+		// F -> Y log 2Q -> Q
+	case 13:
+		__load__1101(cmd, res, log);
+		break;
+		// PR0 -> Y[0,1,2,3] Q-hold
+	case 14:
+		__load__1110(cmd, res, log);
+		break;
+		// F - > Y Q-hold
+	case 15:
+		__load__1111(cmd, res, log);
+		break;
+	}
+	// в res->Y лежит всегда нужный выход
+	_reg_y = res->Y & 0b1111;
+}
+
+void K1804BC2::special(const CommandFields* cmd, ALUReasult* res, ILogger* log)
+{
+	switch (cmd->To)
+	{
+	case 0:
+		__special__0000(cmd, res, log);
+		break;
+	case 2:
+		__special__0010(cmd, res, log);
+		break;
+	case 4:
+		__special__0100(cmd, res, log);
+		break;
+	case 5:
+		__special__0101(cmd, res, log);
+		break;
+	case 6:
+		__special__0110(cmd, res, log);
+		break;
+	case 8:
+		__special__1000(cmd, res, log);
+		break;
+	case 10:
+		__special__1010(cmd, res, log);
+		break;
+	case 12:
+		__special__1100(cmd, res, log);
+		break;
+	case 14:
+		__special__1110(cmd, res, log);
+		break;
+	default:
+		// TODO throw ??
+		break;
+	}
+}
+
+
+VOID K1804BC2::simulate(ABSTIME time, DSIMMODES mode)
+{
+	auto log = new Logger();
+	_time = time;
+
+	if (isPosedge(_pin_T))
+	{
+		// получение микрокоманды
+		auto cmd = getCommand();
+		log->log("CMD #" + std::to_string(_dbg_counter++));
+
+		// загрузка оператов
+		auto ops = getOperands(cmd, log);
+		// вычисление в АЛУ
+		auto result = ALU(cmd->C0, cmd->Alu, ops, log);
+		// выставление флагов
+		// Z - I/O
+		//_reg_z = result->Z;
+		_reg_f3 = result->F3;
+		_reg_c4 = result->C4;
+		_reg_p_ovr = result->P_OVR;
+		_reg_g_n = result->G_N;
+		// загрузка результов
+		load(cmd, result, log);
+		// логирование
+		_inst->log(const_cast<CHAR*>(log->getLog().c_str()));
+
+		delete result;
+		delete ops;
+		delete cmd;
+	}
+
+
+	delete log;
+}
+
+int K1804BC2::getPosition()
+{
+	if (islow(_pin_LSS->istate()))
+		return YOUNG; //младшая
+	else
+	{
+		if (ishigh(_pin_W_MSS->istate()))
+			return MID; // средняя
+		else
+			return OLD; // старшая
+	}
+}
+
+bool K1804BC2::isHigh(IDSIMPIN* pin)
+{
+	return ishigh(pin->istate());
+}
+
+bool K1804BC2::isLow(IDSIMPIN* pin)
+{
+	return islow(pin->istate());
+}
+
+bool K1804BC2::isNegedge(IDSIMPIN* pin)
+{
+	return pin->isnegedge();
+}
+
+bool K1804BC2::isPosedge(IDSIMPIN* pin)
+{
+	return pin->isposedge();
+}
+
+void K1804BC2::setState(ABSTIME time, IDSIMPIN* pin, int set)
+{
+	switch (set)
+	{
+	case 1:
+		pin->setstate(time, 1, SHI);
+		break;
+	case -1:
+		pin->setstate(time, 1, SLO);
+		break;
+	case 0:
+		pin->setstate(time, 1, FLT);
+		break;
+	}
+}
+
+void K1804BC2::computeFlags(ALUReasult* res, bool c0, const Operands* ops, uint8_t aluCode)
+{
+	// TODO
+}
+
+INT K1804BC2::isdigital(CHAR* pinname)
+{
+	return 1;
+}
+
+VOID K1804BC2::runctrl(RUNMODES mode)
+{
+}
+
+VOID K1804BC2::actuate(REALTIME time, ACTIVESTATE newstate)
+{
+}
+
+VOID K1804BC2::callback(ABSTIME time, EVENTID eventid)
+{
+}
+
+BOOL K1804BC2::indicate(REALTIME time, ACTIVEDATA* data)
+{
+	return FALSE;
+}
---
+++ K1804BC2.h
@@ -0,0 +1,166 @@
+#pragma once
+#include <windows.h>
+#include "pch.h"
+#include "vsm.h"
+#include <string>
+#include <cinttypes>
+#include "logger.h"
+
+#define  YOUNG 0 
+#define  MID 1 
+#define  OLD 2 
+const int REGISTER_SIZE = 4;
+const int NUM_OF_REGISTERS = 16;
+const int PIN_I_SIZE = 9;
+
+
+class K1804BC2 : public IDSIMMODEL
+{
+	IINSTANCE* _inst;
+	IDSIMCKT* _ckt;
+	ABSTIME _time;
+
+	uint32_t _dbg_counter = 0;
+	uint8_t _regs[NUM_OF_REGISTERS];
+	uint8_t _reg_q;
+	uint8_t _reg_y;
+	uint8_t _reg_b;
+	bool _reg_z;
+	bool _reg_f3;
+	bool _reg_c4;
+	bool _reg_p_ovr;
+	bool _reg_g_n;
+	IDSIMPIN* _pin_A[REGISTER_SIZE];
+	IDSIMPIN* _pin_B[REGISTER_SIZE];
+	IDSIMPIN* _pin_DA[REGISTER_SIZE];
+	IDSIMPIN* _pin_DB[REGISTER_SIZE];
+	IDSIMPIN* _pin_I[PIN_I_SIZE];
+	IDSIMPIN* _pin_OEY;
+	IDSIMPIN* _pin_OEB;
+	IDSIMPIN* _pin_EA;
+	IDSIMPIN* _pin_EB;
+	IDSIMPIN* _pin_;
+	IDSIMPIN* _pin_T;
+	IDSIMPIN* _pin_C0;
+	IDSIMPIN* _pin_Y[REGISTER_SIZE];
+	IDSIMPIN* _pin_C4;
+	IDSIMPIN* _pin_Z;
+	IDSIMPIN* _pin_F3;
+	IDSIMPIN* _pin_P_OVR;
+	IDSIMPIN* _pin_G_N;
+	IDSIMPIN* _pin_PR0;
+	IDSIMPIN* _pin_PQ0;
+	IDSIMPIN* _pin_PR3;
+	IDSIMPIN* _pin_PQ3;
+	IDSIMPIN* _pin_WE;
+	IDSIMPIN* _pin_IEN;
+	IDSIMPIN* _pin_LSS;
+	IDSIMPIN* _pin_W_MSS;
+	uint8_t genValue(IDSIMPIN** pins, size_t n, size_t offset = 0);
+	bool isHigh(IDSIMPIN* pin);
+	bool isLow(IDSIMPIN* pin);
+	bool isNegedge(IDSIMPIN* pin);
+	bool isPosedge(IDSIMPIN* pin);
+	void setState(ABSTIME time, IDSIMPIN* pin, int set);
+
+	struct CommandFields {
+		uint8_t From;
+		uint8_t Alu;
+		uint8_t To;
+		uint8_t A;
+		uint8_t B;
+		uint8_t DA;
+		uint8_t DB;
+		bool C0;
+	};
+	CommandFields* getCommand();
+
+	struct Operands {
+		uint8_t R;
+		uint8_t S;
+	};
+	Operands* getOperands(const CommandFields* cmd, ILogger* log);
+
+	void __download__000(const CommandFields* cmd, Operands* ops, ILogger* log);
+	void __download__001(const CommandFields* cmd, Operands* ops, ILogger* log);
+	void __download__01X(const CommandFields* cmd, Operands* ops, ILogger* log);
+	void __download__100(const CommandFields* cmd, Operands* ops, ILogger* log);
+	void __download__101(const CommandFields* cmd, Operands* ops, ILogger* log);
+	void __download__11X(const CommandFields* cmd, Operands* ops, ILogger* log);
+
+	struct ALUReasult {
+		uint8_t Y; // Результат Алу
+		bool P_OVR; // Переполнение
+		bool C4; // Перенос из старшего разряда
+		bool F3; // Знак, содержимое старшего разряда АЛУ
+		bool Z; // Признак нулевого результата
+		bool G_N; // Сигнал генерации переноса из АЛУ
+	};
+
+	ALUReasult* ALU(bool c0, uint8_t code, const Operands* ops, ILogger* log);
+
+
+	//I1-4
+	void __alu__0000(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);// I0 важно
+	void __alu__0001(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);
+	void __alu__0010(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);
+	void __alu__0011(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);
+	void __alu__0100(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);
+	void __alu__0101(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);
+	void __alu__0110(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);//I0=0 -> error (reserved func)
+	void __alu__0111(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);//I0=0 -> error (reserved func)
+	void __alu__1000(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);//I0=0 -> error (reserved func)
+	void __alu__1001(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);
+	void __alu__1010(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);
+	void __alu__1011(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);
+	void __alu__1100(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);
+	void __alu__1101(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);
+	void __alu__1110(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);
+	void __alu__1111(bool c0, const Operands* ops, ALUReasult* res, ILogger* log);
+	
+	
+
+	void load(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void special(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	
+	void __load__0000(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__0001(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__0010(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__0011(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__0100(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__0101(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__0110(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__0111(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__1000(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__1001(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__1010(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__1011(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__1100(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__1101(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__1110(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __load__1111(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+
+	// TODO не все коды есть -> сделать исключения ?? по пинам 
+	void __special__0000(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __special__0010(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __special__0100(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __special__0101(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __special__0110(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __special__1000(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __special__1010(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __special__1100(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	void __special__1110(const CommandFields* cmd, ALUReasult* res, ILogger* log);
+	
+	void computeFlags(ALUReasult* res, bool c0, const Operands* ops, uint8_t aluCode); // можно наверное поменять структуру
+	int getPosition();
+public:
+	INT isdigital(CHAR* pinname);
+	VOID setup(IINSTANCE* inst, IDSIMCKT* dsim);
+	VOID runctrl(RUNMODES mode);
+	VOID actuate(REALTIME time, ACTIVESTATE newstate);
+	BOOL indicate(REALTIME time, ACTIVEDATA* data);
+	VOID simulate(ABSTIME time, DSIMMODES mode);
+	VOID callback(ABSTIME time, EVENTID eventid);
+};
+
+
